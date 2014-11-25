df9235b021b903a6f98c4c14d8b19b882d221bbd
SuperTit <andrej83@hotmail.com>
2014-11-25 11:41:36 +0100

    oj vilken röra sista gången

--- FrontEndTentament/WebApplication1/App/Controllers/ListGenresController.js
+++ FrontEndTentament/WebApplication1/App/Controllers/ListGenresController.js
@@ -8,12 +8,16 @@
         $http.delete("/odata/Genres('" + key + "')")
             .success(function () {
                 console.log($scope.DeleteGenre.name + ' ' + 'was deleted.');
-            });
+        });
         console.log('Delete genre');
+        $http.get("/odata/Genres")
+       .success(function (data, status) {
+           $scope.Genres = data.value;
+       });
     };
 
-    $scope.UpdateGenre = function (entity) {
-        $http.put("/odata/Genres:" + "@key")
+    $scope.UpdateGenre = function (genre) {
+        $http.put("/odata/Genres", genre)
             .success(function () {
                 console.log($scope.UpdateGenre.name + ' ' + 'was updated.');
             });
